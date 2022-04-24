c8549b3b61af48d80b4460886028e4b51ba4162b
AdrMAr5 <amarkowski@student.agh.edu.pl>
2022-04-24 16:03:40 +0200

    pointers exercise done

--- pointers.c
+++ pointers.c
@@ -4,22 +4,42 @@
 
 int main(){
     int arr1[10];
-    int* pointer1 = arr1;
+
     double arr2[10];
-    double* pointer2 = arr2;
+
+    // filling the arrays with numbers
     for(int i=0; i<10; i++){
         arr1[i] = 2*i;
         double num = i*3.14;
         arr2[i] = num;
     }
-    printf("array1:\n");
+    printf("array1 address, value\n");
     for(int i=0; i<10; i++){
+        printf("%d, ", &arr1[i]);
+        printf("%d\n",arr1[i]);
+    }
+    printf("\narray2 address, value\n");
+    for(int i=0; i<10; i++){
+        printf("%d, ", &arr2[i]);
+        printf("%f\n",arr2[i]);
+    }
+
+    // printing array1 using pointer
+    int* pointer1 = arr1;
+    printf("\narray1 using pointer:\n");
+    printf("address, value\n");
+    for(int i=0; i<10; i++){
+        printf("%d, ", pointer1);
         printf("%d\n", *pointer1);
         pointer1++;
     }
 
-    printf("array2:\n");
+    // printing array2 using pointer
+    double* pointer2 = arr2;
+    printf("\narray2 using pointer:\n");
+    printf("address, value\n");
     for(int i=0; i<10; i++) {
+        printf("%d, ", pointer2);
         printf("%f\n", *pointer2);
         pointer2++;
     }
